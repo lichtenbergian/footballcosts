068149188bc5e42d05db63113c61b50d7c97aabb
lichtenbergian <mmurgia1307@gmail.com>
2026-01-17 14:44:44 +0100

    Delete football directory

--- football/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Stack } from "./stack";
-export { Heading } from "./heading";
-export { Text } from "./text";
-export { Card } from "./card";
-export { Grid } from "./grid";
-export { Badge } from "./badge";
